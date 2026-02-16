9f90c35f14e0c2aabd2a4fc477db3d8af6e25d4c
Boltlord1 <boltlord1@gmail.com>
2026-02-17 04:52:00 +0500

    removed dotenv

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import path from 'node:path'
-import 'dotenv/config'
 import express from 'express'
 import session from 'express-session'
 import { PrismaSessionStore } from '@quixo3/prisma-session-store'
